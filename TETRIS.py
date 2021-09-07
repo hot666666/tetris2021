174a9d8a2c04907d3d15fd85e3e3e96fba2c35f0
hot666666 <hotcs6@naver.com>
2021-09-08 07:15:02 +0900

    temp

--- TETRIS.py
+++ TETRIS.py
@@ -13,12 +13,17 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 YELLOW = (255, 255, 0)
 
+INVISABLE_BOARD = 2
+DISPLAY_SIZE = (600, 800)
 PAD_SIZE = (400, 800)
 BLOCK_SIZE = 40
-BOARD_ROW = 20
+BOARD_ROW = 20 + INVISABLE_BOARD
 BOARD_COL = 10
 
-SEC = 0.5
+ONE_LINE_SCORE = 200
+ONE_BLOCK_SCORE = 10
+
+SEC = 1
 
 TYPES = [  # index 0~6
     [[0, 0], [0, 1], [0, 2], [0, 3]],  # 직선
@@ -31,13 +36,21 @@ TYPES = [  # index 0~6
 ]
 
 
+from collections import deque
+class Blocks:
+    def __init__(self):
+        self.types = deque([TYPES[random.randint(0, 6)] * 3])
 
+    def popBlock(self):
+        self.types.append(TYPES[random.randint(0, 6)])
+        print(len(self.types))
+        return self.types.popleft()
 
 class Block:
-    def __init__(self):
+    def __init__(self,state):
         self.row = 0
         self.col = int(BOARD_COL * 0.5)
-        self.states = TYPES[random.randint(0, 6)]
+        self.states = state
 
     def minmaxRow(self):
         temp = [row[0] + self.row for row in self.states]
@@ -46,8 +59,9 @@ class Block:
 
 class Board:
     def __init__(self):
-        self.states = [[0] * 10 for _ in range(20)]
-        self.index = [False] * 20
+        self.states = [[0] * 10 for _ in range(20+INVISABLE_BOARD)]
+        self.index = [False] * (20 + INVISABLE_BOARD)
+        self.score = 0
 
     def checkIsFull(self, row):
         if self.states[row] == [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]:
@@ -63,6 +77,7 @@ class Board:
                 cnt += 1
         for _ in range(cnt):
             self.states.insert(0, [0] * 10)
+        self.score += cnt * ONE_LINE_SCORE
 
 
 
@@ -79,24 +94,45 @@ def set_interval(func, sec):  # import threading
 def setBoard():
     for rc in movingBlock.states:
         Board.states[rc[0] + movingBlock.row][rc[1] + movingBlock.col] = 1
-        Board.index[rc[0] + movingBlock.row] = True
+        Board.index[rc[0] + movingBlock.row] = True # 여기 보드에서 손봐줘야함
 
 
 def drawObject(col, row, type):
     pygame.draw.rect(GamePad, type,
                      pygame.Rect((col * BLOCK_SIZE + 2, row * BLOCK_SIZE + 2), (BLOCK_SIZE - 2, BLOCK_SIZE - 2)))
 
+
 def drawBlockOnBoard(color=GREEN):
     for idx, state in enumerate(Board.index):
         if state:
             for col in range(0, BOARD_COL):
                 if Board.states[idx][col] == 1:
-                    drawObject(col, idx, color)
+                    drawObject(col, idx - INVISABLE_BOARD, color)
 
 def drawMovingBlock(colorType, temp=0):  # temp는 낙하지점을 더한 row값
     for y, x in movingBlock.states:
         tx, ty = x + movingBlock.col, y + movingBlock.row + temp
-        drawObject(tx, ty, colorType)
+        drawObject(tx, ty - INVISABLE_BOARD, colorType)
+
+def drawMessage(text, XY,size=20,color=BLACK):
+    global GamePad
+
+    textfont = pygame.font.Font('freesansbold.ttf', size)
+    text = textfont.render(text, True, color)
+    textpos = text.get_rect()
+    textpos.center = XY
+    GamePad.blit(text, textpos)
+    pygame.display.update()
+
+def drawObject2(col, row, type):
+    pygame.draw.rect(GamePad, type,
+                     pygame.Rect((col * 20 + 500, row * 20 + 300), (20-2 , 20-2)))
+def drawQueueBlock(colorType):
+    t = 0
+    for q in Q.types:
+        for y,x in q:
+            drawObject2(x, y+t, colorType)
+        t += 4
 
 def drawBoard():
     for i in range(0, PAD_SIZE[0] + 1, 40):
@@ -132,10 +168,16 @@ def checkFallable(temp):
     return True
 
 
-def down():
+def down():  # 새블럭처리
+    global movingBlock, Q
+
     movingBlock.row += 1
     if not checkInBoard():
+        Board.score += ONE_BLOCK_SCORE
         movingBlock.row -= 1
+        setBoard()
+        Board.checkRows(movingBlock)
+        movingBlock = Block(Q.popBlock())
 
 def fall():
     temp = movingBlock.row
@@ -155,18 +197,24 @@ def rotate():
 
 
 
+
 def runGame():
     global GamePad, BLOCK, Clock
     global Board, movingBlock
 
+
     while True:
         maxRow = fall()
+        if not maxRow:
+            break
 
         GamePad.fill(WHITE)
         drawBoard()
         drawBlockOnBoard()
         drawMovingBlock(YELLOW, maxRow - movingBlock.row)
         drawMovingBlock(GREEN)
+        #drawMessage(f'SCORE: {Board.score} ',(500,100))
+        drawQueueBlock(GREEN)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -190,32 +238,33 @@ def runGame():
                     movingBlock.row += 1
                     if not checkInBoard():
                         movingBlock.row -= 1
-                if event.key == pygame.K_SPACE:
+                if event.key == pygame.K_SPACE:  # 새블록처리
                     movingBlock.row = maxRow
+                    setBoard()
+                    Board.checkRows(movingBlock)
+                    movingBlock = Block(Q.popBlock())
+                    Board.score += ONE_BLOCK_SCORE
 
-        if not checkMovable():
-            setBoard()
-            Board.checkRows(movingBlock)
-            movingBlock = Block()
 
         pygame.display.update()
 
+    pygame.quit()
 
 def initGame():
     global GamePad, BLOCK, Clock
-    global Board, movingBlock
+    global Board, movingBlock, Q
 
     pygame.init()
-    GamePad = pygame.display.set_mode(PAD_SIZE)
+    GamePad = pygame.display.set_mode(DISPLAY_SIZE)
     pygame.display.set_caption('tetris_2021hs_V2')
     BLOCK = pygame.image.load('newblock.png')
 
     pygame.display.set_icon(BLOCK)
     Clock = pygame.time.Clock()
     Board = Board()
-    movingBlock = Block()
+    Q = Blocks()
+    movingBlock = Block(Q.popBlock())
     set_interval(down, SEC)
-
 
 
 
