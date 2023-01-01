aa7a5d85c4c603f240f14f395512a5e603a6aab4
hot666666 <hotcs6@naver.com>
2023-01-02 01:17:00 +0900

    TETRIS2023

--- example/pyame_alpha/alpha_ex.py
+++ example/pyame_alpha/alpha_ex.py
@@ -1,6 +1,6 @@
 import pygame
 
-from tetris.shapes import *
+from ..tetris.shapes import *
 import random
 
 block_size = 30
@@ -14,7 +14,6 @@ grid[11][5] = (0, 255, 0)
 grid[12][5] = (0, 255, 0)
 grid[13][5] = (0, 255, 0)
 
-
 shape = Piece(5, 0, random.choice(shapes))
 
 
@@ -23,14 +22,14 @@ def draw_next_shape(surface):
     sy = 100
     format = shape.shape[shape.rotation % len(shape.shape)]
 
-    temp_size = block_size//2
+    temp_size = block_size // 2
 
     for i, line in enumerate(format):
         row = list(line)
         for j, column in enumerate(row):
             if column == '0':
-                pygame.draw.rect(surface, shape.color, (sx + j*temp_size,
-                                 sy + i*temp_size, temp_size, temp_size), 0)
+                pygame.draw.rect(surface, shape.color, (sx + j * temp_size,
+                                                        sy + i * temp_size, temp_size, temp_size), 0)
 
 
 def draw_block(surface, is_partner=False):
@@ -41,7 +40,7 @@ def draw_block(surface, is_partner=False):
     for i in range(grid_y):
         for j in range(grid_x):
             pygame.draw.rect(surface, grid[i][j],
-                             (sx + j*block_size, sy + i*block_size, block_size, block_size), 0)
+                             (sx + j * block_size, sy + i * block_size, block_size, block_size), 0)
 
 
 def draw_grid(surface, is_partner=False):
@@ -50,17 +49,17 @@ def draw_grid(surface, is_partner=False):
         sx, sy = 2, 2
 
     edge = 4
-    ed = edge//2
+    ed = edge // 2
 
     for i in range(1, grid_y):
         pygame.draw.line(surface, (128, 128, 128),
-                         (sx, sy + i*block_size), (sx+play_width, sy + i*block_size))
+                         (sx, sy + i * block_size), (sx + play_width, sy + i * block_size))
         for j in range(1, grid_x):
             pygame.draw.line(surface, (128, 128, 128),
-                             (sx + j * block_size, sy), (sx + j*block_size, sy + play_height))
+                             (sx + j * block_size, sy), (sx + j * block_size, sy + play_height))
 
     pygame.draw.rect(surface, (196, 196, 196),
-                     (sx-ed, sy-ed, play_width+edge, play_height+edge), edge)
+                     (sx - ed, sy - ed, play_width + edge, play_height + edge), edge)
 
 
 def draw_texts(screen, my_score=0, partner_score=0):
@@ -68,10 +67,10 @@ def draw_texts(screen, my_score=0, partner_score=0):
     font2 = pygame.font.SysFont('comicsans', 20)
 
     label1 = font1.render('Me', 1, (255, 255, 255))
-    screen.blit(label1, (300-label1.get_width() / 2, 850))
+    screen.blit(label1, (300 - label1.get_width() / 2, 850))
 
     label2 = font2.render('Next', 1, (255, 255, 255))
-    screen.blit(label2, (400-label2.get_width()/2, 50))
+    screen.blit(label2, (400 - label2.get_width() / 2, 50))
 
     label3 = font2.render('Score', 1, (255, 255, 255))
     screen.blit(label3, (180, 50))
@@ -80,13 +79,13 @@ def draw_texts(screen, my_score=0, partner_score=0):
     screen.blit(label4, (180, 100))
 
     label1_2 = font1.render(f'Partner', 1, (196, 196, 196))
-    screen.blit(label1_2, (900-label1_2.get_width()/2, 850))
+    screen.blit(label1_2, (900 - label1_2.get_width() / 2, 850))
 
     label3_2 = font2.render('Score', 1, (196, 196, 196))
-    screen.blit(label3_2, (900-label3_2.get_width()/2, 50))
+    screen.blit(label3_2, (900 - label3_2.get_width() / 2, 50))
 
     label4_2 = font1.render(f'{partner_score}', 1, (196, 196, 196))
-    screen.blit(label4_2, (900-label4_2.get_width()/2, 100))
+    screen.blit(label4_2, (900 - label4_2.get_width() / 2, 100))
 
 
 # Initialize Pygame
@@ -102,7 +101,6 @@ screen.fill((0, 0, 0))
 
 # Create a surface for the left rectangle
 left_rect_surface = pygame.Surface((600, 1000))
-
 
 draw_next_shape(left_rect_surface)
 draw_block(left_rect_surface)
@@ -121,7 +119,7 @@ right_rect_inner_surface = pygame.Surface((304, 604))
 draw_block(right_rect_inner_surface, is_partner=True)
 draw_grid(right_rect_inner_surface, is_partner=True)
 right_rect_inner_surface.set_alpha(64)
-right_rect_surface.blit(right_rect_inner_surface, (150-2, 200-2))
+right_rect_surface.blit(right_rect_inner_surface, (150 - 2, 200 - 2))
 
 # Draw the right rectangle on the screen
 screen.blit(right_rect_surface, (600, 0))
