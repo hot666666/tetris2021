970fa7cdbe4d35e47dcde63d6eb180982e9b9164
hot666666 <hotcs6@naver.com>
2024-11-30 11:04:40 +0900

    style : 주석 업데이트

--- tetris2024/game_loop.py
+++ tetris2024/game_loop.py
@@ -2,21 +2,13 @@ import time
 
 
 class GameLoop:
-    def __init__(self, game, renderer, input_event_queue, ui_manager, drop_interval=1.0):
+    def __init__(self, game, renderer, input_event_queue, tk_manager, drop_interval=1.0):
         self.game = game
         self.renderer = renderer
         self.input_event_queue = input_event_queue
-        self.ui_manager = ui_manager
+        self.tk_manager = tk_manager
         self.drop_interval = drop_interval
         self.last_drop_time = time.time()
-
-        self.key_mapping = {
-            "Up": self.game.actions.rotate,
-            "Down": self.game.actions.move_down,
-            "Left": self.game.actions.move_left,
-            "Right": self.game.actions.move_right,
-            "space": self.game.actions.hard_drop,
-        }
 
     def update(self):
         current_time = time.time()
@@ -26,11 +18,11 @@ class GameLoop:
         # 1. 입력 처리
         user_input = self.input_event_queue.pop()
 
-        if user_input not in self.key_mapping:
+        if user_input not in self.tk_manager.key_mapping:
             user_input = None
 
         if user_input:
-            action = self.key_mapping[user_input]
+            action = self.tk_manager.key_mapping[user_input]
 
             gameover, block_respawned = self.game.step(action)
 
@@ -38,22 +30,23 @@ class GameLoop:
             if block_respawned:
                 self.input_event_queue.clear()
 
+        # 자동 드랍 처리
         if delta_time > self.drop_interval:
-            action = self.key_mapping["Down"]
+            action = self.tk_manager.key_mapping["Down"]
             gameover, block_respawned = self.game.step(action)
 
             # 최근 자동 드랍 시간 업데이트
             self.last_drop_time = current_time
 
-        # 3. 렌더링
-        tk_game_img = self.renderer.render(
+        # 2. 렌더링
+        game_img = self.renderer.render(
             self.game.get_game_states()
         )
 
-        # 4. UI(canvas) 업데이트
+        # 3. UI(canvas) 업데이트
         if not gameover:
-            self.ui_manager.update_canvas(tk_game_img)
-            self.ui_manager.canvas.after(16, self.update)
+            self.tk_manager.update_canvas(game_img)
+            self.tk_manager.canvas.after(16, self.update)
         else:
-            self.ui_manager.display_game_over()
-            self.ui_manager.root.after(2000, self.ui_manager.close_window)
+            self.tk_manager.display_game_over()
+            self.tk_manager.root.after(2000, self.tk_manager.close_window)
